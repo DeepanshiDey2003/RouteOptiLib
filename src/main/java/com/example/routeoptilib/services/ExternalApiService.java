85f2097b674cadf4f3ee3243241d322507ca7075
Deepanshi Dey <deepanshi.dey@moveinsync.com>
2025-01-02 15:26:06 +0530

    Added vehicle api

--- src/main/java/com/example/routeoptilib/services/ExternalApiService.java
+++ src/main/java/com/example/routeoptilib/services/ExternalApiService.java
@@ -1,7 +1,40 @@
 package com.example.routeoptilib.services;
 
+import com.example.routeoptilib.utils.Constant;
+import com.moveinsync.vehiclemanagementservice.models.VehicleDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @Service
 public class ExternalApiService {
+    
+    @Autowired
+    private RestTemplate restTemplate;
+    
+    @Value("${vms.api.url}")
+    private String vmsApiUrl;
+    
+    public List<VehicleDTO> getVehiclesByBuid(String buid) {
+        String url = String.format("%s/%s/vehicles", vmsApiUrl, buid);
+        HttpHeaders headers = new HttpHeaders();
+        headers.set(Constant.X_WIS_TOKEN, Constant.X_WIS_STATIC_TOKEN);
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+        
+        try {
+            ResponseEntity<List<VehicleDTO>> response = restTemplate.exchange(url, HttpMethod.GET, entity, new ParameterizedTypeReference<>() {
+            });
+            return response.getBody() != null ? response.getBody() : List.of();
+        } catch (Exception e) {
+            return List.of();
+        }
+    }
 }
--- src/main/java/com/example/routeoptilib/services/RouteService.java
+++ src/main/java/com/example/routeoptilib/services/RouteService.java
@@ -1,10 +1,19 @@
 package com.example.routeoptilib.services;
 
+import com.moveinsync.vehiclemanagementservice.models.VehicleDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class RouteService {
-    public String optimiseRoute() {
+    
+    @Autowired
+    private ExternalApiService externalApiService;
+    
+    public String optimiseRoute(String buid, long startTimestamp, long endTimestamp) {
+        List<VehicleDTO> vehicleDTOList = externalApiService.getVehiclesByBuid(buid);
         return "Optimised Route";
     }
 }
